cc5f4f157bade137b16b49cfdedc9447212b7f2f
Louis <darkenciel31@gmail.com>
2024-01-29 13:12:56 +0100

    first commit

---
+++ src/main/java/fr/daron/louis/App.java
@@ -0,0 +1,15 @@
+package fr.daron.louis;
+
+public class App {
+    
+    public static void main(String[] args) {
+        compteBancaire compte1 = new compteBancaire("Antoine",1000);
+
+        compte1.retirerArgent(600.0);
+        compte1.deposerArgent(150.0);
+        compte1.informationsCompte();
+        compte1.retirerArgent(600.0);
+        compte1.informationsCompte();
+    }
+    
+}
---
+++ src/main/java/fr/daron/louis/compteBancaire.java
@@ -0,0 +1,47 @@
+package fr.daron.louis;
+
+public class compteBancaire {
+    private String nomProprio;
+    public double solde;
+
+    compteBancaire(String nomProprio, double solde){
+        this.solde = solde;
+        this.nomProprio = nomProprio;
+    }
+
+    public String getNomProprio() {
+        return this.nomProprio;
+    }
+
+    public void setNomProprio(String nomProprio) {
+        this.nomProprio = nomProprio;
+    }
+
+    public double getSolde() {
+        return this.solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public void retirerArgent(double somme){
+        if (somme <= solde){
+            solde = solde - somme;
+            System.out.println("Vous avez retiré "+ somme + " euro(s), il vous reste donc " + solde + " euro(s).");
+        }else{
+            System.out.println("Vous ne pouvez pas retirer "+somme+" euro(s) car votre solde est de "+solde+" euro(s)");
+        }
+    }
+
+
+    public void deposerArgent(double somme){
+        solde = solde + somme;
+        System.out.println("Vous avez deposé "+somme+" euro(s), il vous reste donc " + solde + " euro(s).");
+    }
+
+    public void informationsCompte(){
+        System.out.println(" Votre nom est : "+nomProprio+" \n Votre solde est de : "+solde);
+    }
+
+}
